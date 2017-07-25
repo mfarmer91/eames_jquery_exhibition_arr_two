5c1fe895d801ec90b6b45db570c70dd2187d41e7
Michael Farmer <mfarmer91@gmail.com>
2017-07-25 19:13:02 -0400

    updated styling and created overlay -- still needs positioning

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-//$("img").attr("class", "row_of_three");
+//create overlay with image when image is clicked.
+
+$(".description").remove();
+
+//create overlay
+var $overlay = $('<div id="overlay"><div>');
+
+//add an image
+var $image = $("<img>");
+
+$overlay.append("$image");
+$("body").append($overlay);
+
+
+
+//1, disable usual behavior of image.
+
+
+//2, build overlay with image
+
+$("#all_images a").click(function() {
+    event.preventDefault();
+    $overlay.show();
+});
+
+
+$overlay.click(function() {
+  // hide the overlay
+  $overlay.hide();
+  //could also say $(this).hide();
+})
+//
