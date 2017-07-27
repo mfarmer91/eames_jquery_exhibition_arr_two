f66b4dbaa87f772e5eab47eaa902b060741d6ece
Michael Farmer <mfarmer91@gmail.com>
2017-07-27 12:14:55 -0400

    added additional jquery features

--- app.js
+++ app.js
@@ -1,32 +1,64 @@
-//create overlay with image when image is clicked.
+//img_five_overlays
+//var $overlay = $('<div id="overlay"><div>');
+//var $overlay_two = $('<div id="overlay_two"><div>');
 
-$(".description").remove();
+//$("body").append($overlay);
+//$("body").append($overlay_two);
 
-//create overlay
-var $overlay = $('<div id="overlay"><div>');
-
-//add an image
-var $image = $("<img>");
-
-$overlay.append("$image");
-$("body").append($overlay);
+var $eames_one = $("#eames_one");
+var $eames_elephant = $("#eames_elephant");
 
 
 
-//1, disable usual behavior of image.
-
-
-//2, build overlay with image
-
-$("#all_images a").click(function() {
+//img_five_functions
+$("#img_five_link").click(function() {
+    //1, disable usual behavior of image.
     event.preventDefault();
+    
+    $("#eames_one").show();
+//    $("#eames_elephant").show();
     $overlay.show();
+    $overlay_two.show();
+});
+
+$eames_one.click(function() {
+    event.preventDefault();
+    $eames_one.hide();
+    $eames_elephant.hide();
+    $overlay_two.hide();
+})
+
+//$eames_elephant.click(function() {
+//    event.preventDefault();
+//    $eames_one.hide();
+//    $eames_elephant.hide();
+//    $overlay_two.hide();
+//})
+
+
+
+
+
+$("#imageGallery img").click(function() {
+    //1, disable usual behavior of image.
+    event.preventDefault();
+    $(this).hide();
+    $(this).next().show();
+});
+
+$("p").click(function() {
+    event.preventDefault();
+    $(this).hide();
+    $(this).prev().show();
+    
 });
 
 
-$overlay.click(function() {
-  // hide the overlay
-  $overlay.hide();
-  //could also say $(this).hide();
-})
-//
+
+//$("img").click(function() {
+//    //2.2, show text
+//    $(this).next().show();
+//    //2.2, remove img
+//    $(this).remove();   
+//});
+//    
