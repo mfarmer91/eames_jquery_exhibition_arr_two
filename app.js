76c35415147a6d60518b6b0875607eeef74f2d35
Michael Farmer <mfarmer91@gmail.com>
2017-07-29 11:22:34 -0400

    switched out id attributes for variables

--- app.js
+++ app.js
@@ -1,39 +1,29 @@
-//img_five_overlays
-//var $overlay = $('<div id="overlay"><div>');
-//var $overlay_two = $('<div id="overlay_two"><div>');
-
-//$("body").append($overlay);
-//$("body").append($overlay_two);
-
 var $eames_one = $("#eames_one");
 var $eames_elephant = $("#eames_elephant");
+var $imgFiveLink = $("#img_five_link");
+var $eamesText = $("#eames_text");
+var $eamesTextTwo = $("#eames_text_two");
+var $eamesTextThree = $("#eames_text_three");
 
 
 
-//img_five_functions
-$("#img_five_link").click(function() {
-    //1, disable usual behavior of image.
+$imgFiveLink.click(function() {
     event.preventDefault();
-    
-    $("#eames_one").show();
-//    $("#eames_elephant").show();
-    $overlay.show();
-    $overlay_two.show();
+    $eames_one.show();
 });
 
 $eames_one.click(function() {
     event.preventDefault();
     $eames_one.hide();
-    $overlay_two.hide();
 })
 
-$("#eames_text").click(function() {
+$eamesText.click(function() {
     event.preventDefault();
 })
 
 
+
 $("#imageGallery img").click(function() {
-    //1, disable usual behavior of image.
     event.preventDefault();
     $(this).hide();
     $(this).next().show();
@@ -46,23 +36,28 @@ $("p").click(function() {
     
 });
 
+
+//Two text bubbles show or remove for final row of 2.
+
 $(".row_of_two_container a").click(function() {
     //1, disable usual behavior of image.
     event.preventDefault();
-    $("#eames_text_two").show();
-    $("#eames_text_three").show();
+    $eamesTextTwo.show();
+    $eamesTextThree.show();
 }); 
 
-$("#eames_text_three").click(function() {
+$eamesTextTwo.click(function() {
     event.preventDefault();
-    $(this).remove();
+    $eamesTextTwo.remove();
     
 });
 
-$("#eames_text_two").click(function() {
+$eamesTextThree.click(function() {
     event.preventDefault();
-    $(this).remove();
+    $eamesTextThree.remove();
     
 });
+
+
 
 
